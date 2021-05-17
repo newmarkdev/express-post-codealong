e8db6526ed80b14cfce29200252bbb294f5d2efe
newmarkdev <newmarkdev@gmail.com>
2021-05-17 15:02:00 +0200

    post code along

--- server.js
+++ server.js
@@ -7,10 +7,23 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/post-codealong"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Task = mongoose.model('Task', {
+  text: {
+    type:String, 
+    required: true, 
+    minLength: 5
+  }, 
+  complete: {
+    type: Boolean, 
+    default: false
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now
+  }
+});
+
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,6 +35,23 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const {text, complete} = req.body;
+  const task = new Task({text, complete});
+
+  try {
+    const savedTask = await task.save();
+    res.status(201).json(savedTask);
+  }catch (err){
+    res.status(400).json({maessage: 'Could not save task to DB', error: err.errors});
+  }
+});
 
 // Start the server
 app.listen(port, () => {
